bd71b2a108946c3f3e167581720278db2d3cb918
Radtke <dradtke@lm-chi-40007216.paypalcorp.com>
2019-12-02 16:24:21 -0600

    Refactor repo to use stow

---
+++ README.md
@@ -0,0 +1,3 @@
+# damien's dotfiles
+
+This repo contains my dotfiles, managed with [`stow`](https://www.gnu.org/software/stow/).
--- install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-DIR="$(dirname "${BASH_SOURCE[0]}" )"
-cp "${DIR}/gitconfig" "${HOME}/.gitconfig"
-cp "${DIR}/gitignore" "${HOME}/.gitignore"
-cp "${DIR}/tmux.conf" "${HOME}/.tmux.conf"
-cp "${DIR}/vimrc" "${HOME}/.vimrc"
-cp -a "${DIR}/vim" "${HOME}/.vim"
